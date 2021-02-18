ab272edc99d970fa3c28529ebf7a0ff90256654b
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-02-18 21:16:38 +0700

    WIP 2021

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,13 @@
 alias project-tree "exa --git-ignore --tree --all -I .git"
+alias docker-compose podman-compose
 
 starship init fish | source
+/home/linuxbrew/.linuxbrew/bin/brew shellenv | source
 for dir in /opt/asdf-vm ~/.asdf
   test -d $dir && source $dir/asdf.fish && break
 end
 
 set fish_greeting
+
+source ~/.asdf/asdf-vm/asdf.fish
+
