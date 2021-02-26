6b4b1cb27124955a1cfc6a4fda9d4b8beb1189bd
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-02-26 15:19:56 +0700

    Add func to create+mix virtual output with system audio

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,9 +6,24 @@ alias docker-compose podman-compose
 
 starship init fish | source
 /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
+
 for dir in /opt/asdf-vm ~/.asdf
   test -d $dir && source $dir/asdf.fish && break
 end
+
+
+function route-audio
+    echo "create+mix virtual output $argv[1] with system output"
+
+    pactl load-module module-device-manager
+    pactl load-module module-null-sink sink_name=$argv[1]
+    pactl load-module module-combine-sink sink_name=alsa_output.$argv[1] \
+        slaves=$argv[1],alsa_output.pci-0000_00_1f.3.analog-stereo
+
+    echo DONE
+    echo "please open pavucontrol to rename $argv[1] sink"
+end
+
 
 set fish_greeting
 
