23a4fc9037e96b9c08e82734661f527b2b127c8b
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-03-02 00:51:34 +0700

    Update my OBS profile

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,15 +13,43 @@ end
 
 
 function route-audio
-    echo "create+mix virtual output $argv[1] with system output"
+  echo "create+mix virtual output $argv[1] with system output"
 
-    pactl load-module module-device-manager
-    pactl load-module module-null-sink sink_name=$argv[1]
-    pactl load-module module-combine-sink sink_name=alsa_output.$argv[1] \
-        slaves=$argv[1],alsa_output.pci-0000_00_1f.3.analog-stereo
+  pactl load-module module-device-manager
+  pactl load-module module-null-sink sink_name=$argv[1]
+  pactl load-module module-combine-sink sink_name=alsa_output.$argv[1] \
+      slaves=$argv[1],alsa_output.pci-0000_00_1f.3.analog-stereo
 
-    echo DONE
-    echo "please open pavucontrol to rename $argv[1] sink"
+  echo DONE
+  pactl list short sinks
+  echo "please open pavucontrol to rename '$argv[1]' sink"
+end
+
+
+function prepare-chat
+  switch $argv[1]
+      case twitch
+          set -l scopes chat:edit \
+                        # channel:moderate \
+                        # channel_commercial \
+                        # channel_editor \
+                        # whispers:edit \
+                        chat:read
+          # set -l token (twitch token -u -s "$scopes")
+          # echo $token
+          twitch token -u -s "$scopes"
+      case '*'
+          echo "$argv[1] chat not supported"
+  end
+end
+
+
+function prepare-streaming
+  bspc rule -a obs desktop='^0' follow=on
+  bspc rule -a "kitty:*:twitch-chat drsensor" desktop='^0' state=floating
+  kitty --instance-group stream -- (pushd && twitch-chat drsensor; popd)
+  route-audio music
+  prime-run obs & && pavucontrol &
 end
 
 
--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,12 +1,21 @@
 {
   # https://nixos.org/manual/nixpkgs/stable/#sec-declarative-package-management
   packageOverrides = pkgs: with pkgs; {
+    myWorkflow = buildEnv {
+      name = "workflow-utils";
+      paths = [
+        taskwarrior # todo manager
+        taskwarrior-tui
+      ];
+    };
+
     myIDE = buildEnv {
       name = "development-environment";
       paths = let
         editor = [
           # normal editor
-          micro
+          micro # tui
+          lite # gui
           # modal editor
           kak
           kak-lsp
@@ -35,6 +44,8 @@
           nushell # power-user shell
           nix-direnv # auto-switch environment
           broot # fuzzy search
+          zoxide # `cd` command that learns
+          python39Packages.howdoi # intellicode as cli
         ];
         essential = [
           watchexec # watch change then run command
@@ -54,26 +65,62 @@
       ;
     };
 
-    myWebdev = {
+    myWebdev = buildEnv {
       name = "webdev-toolkit";
       paths = with nodePackages_latest;
-        [ h2o nodejs pnpm closurecompiler ];
+        [
+          devd
+          h2o
+          # hurl
+          # shell2http
+          websocketd
+          websocat
+        ] ++ [ nodejs pnpm closurecompiler ];
+    };
+
+    mySysdev = buildEnv {
+      name = "sysdev-toolkit";
+      paths = [ rust go zig nim ]
+      ++ [ xmake sccache binutils lld ]
+        # ++ [ ld valgrind-light perf-tools bcal hexyl elfutils elfinfo ] # inspect
+        # ++ [ hotspot elf-dissector rehex massif-visualizer ] # GUI for inspect
+      ;
+    };
+
+    myScripting = buildEnv {
+      name = "scripting-toolkit";
+      paths = [
+        # bpkg
+        # lua
+        # luajitPackages.luarocks-nix
+        # python3
+        # pypi2nix
+      ];
+    };
+
+    myEmu = buildEnv {
+      name = "cross-platform-emulation";
+      paths = [ qemu kvm wine darling-dmg virt-manager ];
     };
 
     # currently not in nixpkgs
     # blockbench dust3d pixelorama material-maker butler
 
-    myGamedev = {
+    myGamedev = buildEnv {
       name = "gamedev-toolkit";
       paths = let
-        game-engine = [ godot ];
+        game-engine = [
+          love
+          godot
+          # babylonjs-editor
+        ];
         shader-material = [
           shadered # shader IDE
           # material-maker
         ];
         pixel-art = [
-          # aseprite-unfree # pixel-art editor
           # pixelorama
+          # aseprite-unfree # pixel-art editor
           xprite-editor # pixel-art editor
           pikopixel # pixel-art editor
           rx # pixel editor
@@ -81,69 +128,108 @@
         ];
         math = [
           zegrapher # plot equation
-          manim # animation engine for explanatory math
           plotutils # collection of plot tools
-          openscad # parametric CAD modeling
+          # manim # animation engine for explanatory math
+          # mathinspector # visual programming environment for math
+          # openscad # parametric CAD modeling
         ];
         release = [
           # butler # itch.io
         ];
       in
         game-engine
+        # ++ pixel-art
         ++ shader-material
         ++ math
       ;
     };
 
-    myArt = {
+    myArt = buildEnv {
       name = "art-toolkit";
       paths = let
 
         _3d-model = [
-          goxel # voxel editor
           # blockbench
-          freecad # CAD editor
-          sweethome3d.application # interior design
+          goxel # voxel editor
+          # freecad # CAD editor
+          # sweethome3d.application # interior design
         ];
         _3d-character = [
           # dust3d
         ];
-        _2d-animation = [ opentoonz synfigstudio ];
-        _2d-drawing = [ krita inkscape ];
+        _3d = []
+        ++ [ blender ]
+        ++ _3d-model
+        ++ _3d-character
+        ;
 
-        _3d = [ blender ] ++ _3d-model;
-        _2d = _2d-animation ++ _2d-drawing;
+        _2d-animation = [
+          opentoonz
+          synfigstudio
+        ];
+        _2d-drawing = [
+          # krita
+          inkscape
+        ];
+        _2d = []
+        ++ _2d-drawing
+          # ++ _2d-animation
+        ;
+
         misc = [
           gimp # image editor
-          opencolorio # color management framework
+          # opencolorio # color management framework
         ];
       in
         _3d ++ _2d
       ;
     };
 
-    myStream = {
+    # TODO: package obs.input-overlay
+    # https://github.com/univrsal/input-overlay
+    myStream = buildEnv {
       name = "youtuber-toolkit";
       paths = let
+        chat = let
+          #TODO: use packageOverride like in nix python pills
+          weechatWithScripts = with weechatScripts;
+            [
+              # cosmetics
+              colorize_nicks
+              weechat-autosort
+              weechat-notify-send
+
+              #platform
+              # weechat-twitch
+            ] ++ [ weechat ];
+        in
+          [ weechatWithScripts irssi ];
         record = [
           # screen recorder
+          # obs-browser # https://github.com/obsproject/obs-browser/issues/219#issuecomment-773240464
+          # obs-text-pango # https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=obs-text-pango#n27
+          # obs-input-overlay # https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=obs-input-overlay-bin#n20
           obs-gstreamer
+          obs-v4l2sink
           simplescreenrecorder # support OpenGL recording
           # terminal recorder
           asciinema
-          termtosvg
+          # termtosvg
         ];
         download = [ youtube-dl tartube ];
         edit = [
+          # lossless-cut # cut,split,trim video
           audacity # audio editor
           blender # video editor
-          opencolorio # color management
-          tts # mozilla deep-learning text-to-speech
+          # opencolorio # color management
+          # tts # mozilla deep-learning text-to-speech
           mimic # mycroft machine-learning text-to-speech
-          talkfilters # English text to humoruous text
+          # talkfilters # English text to humoruous text
         ];
       in
-        record ++ edit;
+        record ++ edit
+        ++ download
+      ;
     };
   };
 }
